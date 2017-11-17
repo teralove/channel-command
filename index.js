01c0479c6b9a663394ed5e5133cabd68be8867c8
seraphinush <30970630+seraphinush@users.noreply.github.com>
2017-11-17 14:58:53 +0900

    Update index.js

--- index.js
+++ index.js
@@ -4,19 +4,25 @@
 
 module.exports = function ChannelCommand(dispatch) {
 	
-	let currentChannel = null
+	let currentChannel = 0
 	
 	// command
 	try {
 		const Command = require('command')
 		const command = Command(dispatch)
-		command.add(['channel', 'ch', 'c'], (num) => { changeChannel(num) })
+		command.add(['channel', 'ch', 'c'], (num) => { 
+			if (isNaN(num)) {
+				send(`<font color="#FF0000">Invalid argument.</font>`)
+			} else {
+				changeChannel(num)
+			}
+		})
 	} catch (e) {
 		console.log(`[ERROR] -- channel-command module --`)
 	}
 
 	// code
-	dispatch.hook('S_CURRENT_CHANNEL', 2, (event) => { currentChannel = event })
+	dispatch.hook('S_CURRENT_CHANNEL', (event) => { currentChannel = event })
 	
 	// helper
 	function changeChannel(newChannel) {
@@ -24,11 +30,11 @@ module.exports = function ChannelCommand(dispatch) {
 		// Changing channels inside an instance will teleport the player out to the entrance/teleportal of the instance.
 		if (currentChannel.channel > 20) return
 		// proceed if the argument is a number.
-		if (isNaN(newChannel)) return
+		//if (isNaN(newChannel)) return
 		// decrement by 1, because C_SELECT_CHANNEL identifies channel 1 as 0
 		newChannel -= 1
 
-		dispatch.toServer('C_SELECT_CHANNEL', 1, {
+		dispatch.toServer('C_SELECT_CHANNEL', {
 			unk: 1,
 			zone: currentChannel.zone,
 			channel: newChannel
